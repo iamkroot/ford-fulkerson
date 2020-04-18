e79c04fdfb10fe6436dffe4c58ef3074d38a66c3
Shantanu23Gupta <f20170137@hyderabad.bits-pilani.ac.in>
2020-04-18 18:50:32 +0530

    Add doxygen comments

--- Graph.h
+++ Graph.h
@@ -5,6 +5,10 @@
 #include <unordered_map>
 #include <vector>
 
+/**
+ * @struct Edge
+ * @brief Representation of a directed edge
+ */
 template<typename T>
 struct Edge {
     T u{};
@@ -21,8 +25,17 @@ struct Edge {
     }
 };
 
+/**
+ * @struct EdgeHash
+ * @brief Hash Generator for Edge
+ */
 template<typename T>
 struct EdgeHash {
+
+    /**
+     * @brief () operator overriding
+     * @return Hash of given edge
+     */
     inline std::size_t operator()(const Edge<T> &edge) const {
         return std::hash<T>{}(edge.u) / 2 + std::hash<T>{}(edge.v) / 2;
     }
@@ -34,6 +47,10 @@ using AdjLst = std::unordered_map<T, std::unordered_set<T>>;
 template<typename T>
 using EdgeMap = std::unordered_map<Edge<T>, unsigned int, EdgeHash<T>>;
 
+/**
+ * @class Graph
+ * @brief Get the maxFlow and minCut by repeatedly finding augmenting paths through the residual graphs
+ */
 template<typename T>
 class Graph {
     int numEdges{0};
@@ -44,24 +61,69 @@ class Graph {
     EdgeMap<T> flow;
 
 public:
-
+    /**
+     * @brief Initialize an empty graph
+     */
     Graph();
 
+    /**
+     * @brief Get edge capacities of the graph
+     * @return Edge capacities
+     */
     const EdgeMap<T> &getEdgeCapacities() const;
 
+    /**
+     * @brief Add a vertex to the graph
+     * @param v Vertex to be added
+     */
     void addVertex(T v);
 
+    /**
+     * @brief Add an edge to the graph
+     * @param u Vertex from which the edge originates 
+     * @param v Vertex on which the edge terminates
+     * @param capacity Edge Capacity 
+     */
     void addEdge(T u, T v, unsigned int capacity);
 
+    /**
+     * @brief Get a path from source to target
+     * @param source Source Vertex
+     * @param target Target Vertex 
+     * @return Path from source to target
+     */
     std::vector<Edge<T>> getPath(T source, T target);
 
+    /**
+     * @brief Augment the chosen path
+     * @param path Path to be augmented
+     * @return Augmented path
+     */
     void augment(std::vector<Edge<T>> path);
 
+    /**
+     * @brief Get the maximum flow value possible from source to target
+     * @param source Source Vertex
+     * @param target Target Vertex
+     * @return Maximum flow value
+     */
     EdgeMap<T> maxFlow(const T &source, const T &target);
 
+    /**
+     * @brief Get the minimum st-cut
+     * @param source Source Vertex
+     * @param target Target Vertex
+     * @return Minimum st-cut value
+     */
     std::pair<std::unordered_set<T>, std::unordered_set<T>> minCut(const T &source, const T &target);
 };
 
+/**
+     * @brief Check if flow is valid
+     * @param flow The flow value of the edge
+     * @param edgeCapacities The respective edge capacity values
+     * @return True if flow is valid else False
+     */
 template<typename T>
 bool checkFlow(const EdgeMap<T> &flow, const EdgeMap<T> &edgeCapacities) {
     std::unordered_map<T, unsigned int> out, in;
--- Utils.h
+++ Utils.h
@@ -3,8 +3,18 @@
 
 #include "Graph.h"
 
+/**
+ * @brief Read flow graph information from file
+ * @param path Path to file containing graph
+ * @return Tuple of Graph (initialized as per file), source vertex and target vertex
+ */
 std::tuple<Graph<int>, int, int> readFlowGraph(const std::string &path);
 
+/**
+ * @brief Read bipartite graph information from file
+ * @param path Path to file containing bipartite graph
+ * @return Tuple of Graph (initialized as per file), source vertex and target vertex
+ */
 std::tuple<Graph<int>, int, int, int> readBipartiteGraph(const std::string &path);
 
 #endif //UTILS_H
